ae30d994996e95ee745333b8b879018b374ee78f
conmeno <conmeno@outlook.com>
2016-02-11 22:27:38 +0700

    ggg

--- Conway's Game of Life/AdManager/Data.swift
+++ Conway's Game of Life/AdManager/Data.swift
@@ -19,11 +19,25 @@ class Data {
 //    
 //    cf79991e19d1de3c602113f03fba1b5d5c241cbe
     
-        let gBanner : String = "ca-app-pub-8053657021933536/8397474403"
-        let gFull : String = "ca-app-pub-8053657021933536/9874207607"
+        let gBanner : String = "ca-app-pub-2767935542217864/4801527832"
+        let gFull : String = "ca-app-pub-2767935542217864/6278261035"
         let TestDeviceID: String = "12d7a54f9646b17dab2a691b5bb24e43"
-        let cAppID: String = "56b451bb04b0161b3830b96b"
-        let cSign : String = "cf79991e19d1de3c602113f03fba1b5d5c241cbe"
+        let cAppID: String = "56bc87e804b01664a9e8dc39"
+        let cSign : String = "603ebca4668b11528027d0edb74150f43f12d69f"
+    
+    
+//    App name: Game Of Life 1970
+//    
+//    Ad unit name: Game of life banner
+//    Ad unit ID: ca-app-pub-2767935542217864/4801527832
+//    
+//    Ad unit name: Game of life full
+//    Ad unit ID: ca-app-pub-2767935542217864/6278261035
+//    App ID
+//    56bc87e804b01664a9e8dc39
+//    Signature
+//    603ebca4668b11528027d0edb74150f43f12d69f
+
     
     
 }
